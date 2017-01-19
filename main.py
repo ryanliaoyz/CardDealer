5623934d4bc4d2cf1dfd8fe49df862988804e94d
unknown <>
2017-01-19 09:44:30 -0500

    template here...

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from random import shuffle
 
+suits = []
+ranks = []
+
+deck = []
+
+for suit in suits:
+    for rank in ranks:
+        deck.append(rank + ' ' + suit)
+
+shuffle(deck)
+print(deck)
+
+hand1 = deck.pop() + ' ' + deck.pop() #remove the last one
+hand2 = deck.pop() + ' ' + deck.pop()
